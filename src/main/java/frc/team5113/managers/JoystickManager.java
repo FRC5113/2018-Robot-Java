58313069f1431b80b359cdad3d19671a76908bb1
Combustible Lemons 5113 <frc5113@gmail.com>
2018-01-16 19:45:26 -0500

    drivetrain almost done

---
+++ src/main/java/frc/team5113/managers/JoystickManager.java
@@ -0,0 +1,5 @@
+package frc.team5113.managers;
+
+public class JoystickManager {
+
+}
---
+++ src/main/java/frc/team5113/subsytems/DriveTrain.java
@@ -0,0 +1,34 @@
+package frc.team5113.subsytems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+public class DriveTrain
+{
+	WPI_TalonSRX leftRear;
+	WPI_TalonSRX leftFront;
+	WPI_TalonSRX rightRear;
+	WPI_TalonSRX rightFront;
+	
+	public void init()
+	{
+		leftRear = new WPI_TalonSRX(0);
+		leftFront = new WPI_TalonSRX(1);
+		rightRear = new WPI_TalonSRX(2);
+		rightFront = new WPI_TalonSRX(3);
+		
+		rightRear.setInverted(true);
+		rightFront.setInverted(true);
+	}
+	
+	public void drive(double leftPower, double rightPower)
+	{
+		leftPower *= 0.99;
+		rightPower *= 0.99;
+		leftRear.set(leftPower);
+		leftFront.set(leftPower);
+		rightRear.set(rightPower);
+		rightFront.set(rightPower);
+	}
+	
+	
+}
