2fb66713409892ab9e263fa8984239b4e5658e21
Combustible Lemons 5113 <frc5113@gmail.com>
2018-02-05 10:53:48 -0500

    idk

--- src/main/java/frc/team5113/managers/AutonManager.java
+++ src/main/java/frc/team5113/managers/AutonManager.java
@@ -3,6 +3,7 @@ package frc.team5113.managers;
 import frc.team5113.auton.AutonCommands;
 import frc.team5113.auton.MiddleSwitch;
 import frc.team5113.subsytems.DriveTrain;
+import frc.team5113.subsytems.PID_Controller;
 
 public class AutonManager
 {
@@ -10,6 +11,8 @@ public class AutonManager
 	private final int NUM_OF_CASES = 0;
 	AutonCommands command;
 	MiddleSwitch middleSwitch;
+	private int currentState;
+	
 	public void init()
 	{
 		selectedCase = 1;
@@ -21,24 +24,29 @@ public class AutonManager
 		
 	}
 	
-	public void update(DriveTrain dt, SensorManager sm)
+	/*
+	 * needs to return angle and speed
+	 * needs to get 3 letter code
+	 * 
+	 */
+	public void update()
 	{
-		//command.update(dt, sm);
-		middleSwitch.update(command);
-		/*switch(selectedCase)
+		switch(currentState)
 		{
-		default:
+			case 0:
+				//check current location and 3 letter code
+				currentState = 1;
 			break;
-		}*/
-	}
-	
-	public void setCase(int autonCase)
-	{
-		this.selectedCase = autonCase % NUM_OF_CASES;
-	}
-	
-	public int getCase()
-	{
-		return selectedCase;
+			
+			case 1:
+				//drive forward at 0 angle a distance
+				currentState = 2;
+			break;
+			
+			case 2:
+				//check the distance
+				//if over line stop
+			break;
+		}
 	}
 }
